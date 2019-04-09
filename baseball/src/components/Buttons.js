b6853cbaa0832681632daa9181cbbf5e9d9e57fa
ddot727 <ddot727@gmail.com>
2019-04-09 18:52:35 -0400

    butild functionality of buttons according to given logic

--- baseball/src/components/Buttons.js
+++ baseball/src/components/Buttons.js
@@ -1,19 +1,34 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class Buttons extends Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.strike}>Strike</button>
-        &nbsp;
-        <button onClick={this.ball}>Ball</button>
-        &nbsp;
-        <button onClick={this.foul}>Foul</button>
-        &nbsp;
-        <button onClick={this.hit}>Hit!</button>
-      </div>
-    );
-  }
-}
+// class Buttons extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.strike}>Strike</button>
+//         &nbsp;
+//         <button onClick={this.ball}>Ball</button>
+//         &nbsp;
+//         <button onClick={this.foul}>Foul</button>
+//         &nbsp;
+//         <button onClick={this.hit}>Hit!</button>
+//       </div>
+//     );
+//   }
 
-export default Buttons;
+//   strike = e => {
+//     if (this.state.strikes < 3) {
+//       this.setState({
+//         ...state,
+//         strikes: this.state.strikes + 1
+//       });
+//     } else {
+//       this.setState({
+//         ...state,
+//         strikes: 0,
+//         balls: 0
+//       });
+//     }
+//   };
+// }
+
+// export default Buttons;
--- baseball/src/components/Dashboard.js
+++ baseball/src/components/Dashboard.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Display from "./Display";
-import Buttons from "./Buttons";
 
 class Dashboard extends Component {
   state = {
@@ -13,10 +12,80 @@ class Dashboard extends Component {
     return (
       <div>
         <Display state={this.state} />
-        <Buttons state={this.state} />
+        <button onClick={this.strike}>Strike</button>
+        &nbsp;
+        <button onClick={this.ball}>Ball</button>
+        &nbsp;
+        <button onClick={this.foul}>Foul</button>
+        &nbsp;
+        <button onClick={this.hit}>Hit!</button>
       </div>
     );
   }
+
+  strike = e => {
+    if (this.state.strikes < 3) {
+      this.setState({
+        ...this.state,
+        strikes: this.state.strikes + 1
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        strikes: 0,
+        balls: 0,
+        fouls: 0,
+        hits: 0
+      });
+    }
+  };
+
+  ball = e => {
+    if (this.state.balls < 4) {
+      this.setState({
+        ...this.state,
+        balls: this.state.balls + 1
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        strikes: 0,
+        balls: 0,
+        fouls: 0,
+        hits: 0
+      });
+    }
+  };
+
+  foul = e => {
+    if (this.state.strikes === 0 || this.state.strikes === 1) {
+      this.setState({
+        ...this.state,
+        strikes: this.state.strikes + 1,
+        fouls: this.state.fouls + 1
+      });
+    } else if (this.state.fouls < 4) {
+      this.setState({
+        ...this.state,
+        fouls: this.state.fouls + 1
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        strikes: 0,
+        balls: 0,
+        fouls: 0,
+        hits: 0
+      });
+    }
+  };
+
+  hit = e => {
+    this.setState({
+      ...this.state,
+      hits: this.state.hits + 1
+    });
+  };
 }
 
 export default Dashboard;
